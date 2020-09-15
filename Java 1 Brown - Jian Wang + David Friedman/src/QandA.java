aaff7c8aaee46e893b20dca85a7f0b54d34fcd29
jasonwang <jasonwang@10.0.0.135>
2020-09-15 14:07:27 -0400

    4 questions now

--- Java 1 Brown - Jian Wang + David Friedman/src/QandA.java
+++ Java 1 Brown - Jian Wang + David Friedman/src/QandA.java
@@ -1,16 +1,20 @@
-
+//Question and Answers Project
+//by Jason Wang, with reference of Mr. David's example
 public class QandA {
 
 	
 	public static void main(String[] args) {
-	
+	//Q1
 	System.out.println("Question1");
-	
-	
-	
-	
-	
-	
-	
+	System.out.println("Answer1");
+	//Q2
+	System.out.println("Question2");
+	System.out.println("Answer2");
+	//Q3
+	System.out.println("Question3");
+	System.out.println("Answer3");
+	//Q4
+	System.out.println("Question4");
+	System.out.println("Answer4");
 	}
 }
