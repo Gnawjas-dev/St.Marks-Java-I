2f04cc5d582d15798930c5a5856aff61e6783e86
jasonwang <jasonwang@10.0.0.135>
2020-09-17 07:00:21 -0400

    4 questions, spaced, ending w/ joke

--- Java 1 Brown - Jian Wang + David Friedman/src/QandA.java
+++ Java 1 Brown - Jian Wang + David Friedman/src/QandA.java
@@ -1,20 +1,31 @@
 //Question and Answers Project
-//by Jason Wang, with reference of Mr. David's example
+//by Jason Wang
 public class QandA {
 
 	
 	public static void main(String[] args) {
 	//Q1
-	System.out.println("Question1");
-	System.out.println("Answer1");
+	System.out.println("What is your favorite snacks?");
+	System.out.println("I prefer salty snacks, and crunchy ones too. In general, I like chips either with or with out dips.");
 	//Q2
-	System.out.println("Question2");
-	System.out.println("Answer2");
+	System.out.println(" ");
+	System.out.println("What is your favorite food?");
+	System.out.println("I like steak, steak is always a good option to choose. Otherwise I just go for the good ol' souprice. The"
+			+" soup rice is one of my favorites because it's soft and flavorful and gives me a good warm breakfast.");
 	//Q3
-	System.out.println("Question3");
-	System.out.println("Answer3");
+	System.out.println(" ");
+	System.out.println("What is your favorite anime show?");
+	System.out.println("Jojo's Bizzare Adventures, it's about a group of teens that unlock mysterious powers and race to take down"
+			+" the villain of the season/serie. They are at times funny and at times sad and that's why I enjoyed this show");
 	//Q4
-	System.out.println("Question4");
-	System.out.println("Answer4");
+	System.out.println(" ");
+	System.out.println("What do you do in your free time?");
+	System.out.println("Oh I guess I'm a bit dull, I just surf on Youtube and read books, otherwise I'll just be playing games [by"
+			+" that I mean ONLY Team Fortress 2]. Funfact: I just got to 1k hours on that game, and the game itself is 10+ years old!");
+	
+	//End
+	System.out.println(" ");
+	System.out.println("Alright that's all the questions I can think of right now, if you have more questions reach out to me @:"
+			+" JianWang@stmarksschool.org, don't forget to like, subscribe, oh and go to Patreon and give me money... yes.");
 	}
 }
